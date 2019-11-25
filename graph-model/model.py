b16a90fddcacad2930dce8210787f4e7aa962b33
Maël <mael.nogues@ecole-navale.fr>
2019-11-25 16:39:43 +0100

    testing neo4j on python

---
+++ graph-model/model.py
@@ -0,0 +1,28 @@
+# driver = GraphDatabase.driver(
+#    'bolt://localhost:7687', auth=('neo4j', 'iN2WuicrfZZ3s7d'))
+
+
+class Machine:
+    def __init__(self, name, ip):
+        self.__name, self.__ip = name, ip
+
+    def create_machine(driver, name, ip):
+        with driver.session as session:
+            session.run('create (' + name + ':Machine {name:\'' + name
+                        + '\', ip:\'' + ip + '\'})')
+        return Machine(name, ip)
+
+    def create_connection(self, driver, machine, protocol, rate):
+        with driver.session as session:
+            session.run('match (a:Machine), (b:Machine) where a.name = \''
+                        + self.__name + '\' and b.name = \''
+                        + machine.name +
+                        '\' create (a)-[r:EXCHANGED {protocol: \''
+                        + protocol + '\', rate:\'' + rate + '\'}]->(b)')
+
+    # Attributes
+    @property
+    def name(self): return self.__name
+
+    @property
+    def ip(self): return self.__ip
---
+++ graph-model/populate.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import argparse
+import json
+
+from neo4j import GraphDatabase
+
+from . import model
+
+if __name__ == "__main__":
+    # Argument parsing
+    parser = argparse.ArgumentParser(
+        description='Python framework for an easy creation of a network model')
+    parser.add_argument('-u', '--user', nargs='?', default='neo4j',
+                        help='The user to connect as')
+    parser.add_argument('-p', '--password', nargs='?', default='neo4j',
+                        help='The password to connect to neo4j')
+    parser.add_argument('-a', '--address', nargs='?',
+                        default='bolt://localhost:7687',
+                        help='The url to connect to the server')
+    parser.add_argument('json', nargs='+',
+                        help='JSON file containing the network to graph')
+    args = parser.parse_args()
+
+    # Initialisation
+    driver = GraphDatabase.driver(
+        args.bolt_address, auth=(args.user, args.password))
+
+    network = {}
+    with open(args.json, 'r') as f:
+        network = json.load(f)
+
+    # Processing
+    machines = {}
+    for machine in network:
+        machines[machine] = model.Machine.create_machine(
+            driver, network[machine]['name'], network[machine]['ip'])
+
+    for machine in network:
+        for rel in network[machine]['relations']:
+            for protocol in network[machine]['relations'][rel]:
+                machines[machine].create_connection(
+                    driver, machines[rel], protocol,
+                    network[machine]['relations'][rel][protocol]
+                )
