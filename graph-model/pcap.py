e312923111c75fb85979fc62108b3445abd10063
Maël <mael.nogues@ecole-navale.fr>
2019-12-03 16:34:09 +0100

    update

--- graph-model/pcap.py
+++ graph-model/pcap.py
@@ -1,6 +1,19 @@
-import scapy
+from scapy.all import *
+from neo4j import GraphDatabase
 import argparse
 import json
+import populate
+
+
+def pcap_to_json(pcap):
+    network = {}
+    src_dst = []
+    for packet in pcap:
+        if IP in packet:
+            src_dst += [(packet[IP].src, packet[IP].dst, packet[IP])]
+
+    return network
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -13,6 +26,13 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--address', nargs='?',
                         default='bolt://localhost:7687',
                         help='The url to connect to the server')
+    parser.add_argument('pcap', nargs='+',
+                        help='PCAP file containing the network to graph')
     args = parser.parse_args()
 
-    
+    # Initialisation
+    driver = GraphDatabase.driver(
+        args.address, auth=(args.user, args.password))
+
+    for pcap in args.pcap:
+        populate.network_import(driver, pcap_to_json(rdpcap(pcap)))
